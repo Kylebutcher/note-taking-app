58e74fa5a6a4accc79cdabfb496318615e9dd4f9
kyle butcher <kylembutcher97@gmail.com>
2024-05-09 18:22:29 -0500

    added delete route

--- server.js
+++ server.js
@@ -3,10 +3,13 @@ const path = require('path');
 const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
 
-const data = require('./db/db.json');
+let data = require('./db/db.json');
 const PORT = 3002;
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 // sets up html routes
 app.get('/', (req, res) => {
@@ -28,10 +31,18 @@ app.post('/api/notes', (req, res) => {
   res.json(data);
 });
 
+app.delete("/api/notes/:id", (req, res) => {
+  data = data.filter(({id}) => id !== req.params.id)
+  fs.writeFile(`./db/db.json`, JSON.stringify(data), (err) => {
+    err ? console.log(err) : console.log('Success')
+  })
+  res.json(data);
+});
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/index.html'));
 });
+
 
 app.listen(PORT, () => 
   console.log(`Example app listening at http://localhost:${PORT}`)); 
